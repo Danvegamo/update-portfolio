c27072cacb9c6aae227b3e8f35e4492908205f6c
David Vega <102634786+Danvegamo@users.noreply.github.com>
2023-08-28 20:36:01 +0000

    initial commit

--- site.config.ts
+++ site.config.ts
@@ -2,27 +2,28 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '016e0ef793884b01a8bee4d024a6591c',
+  
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'David Vega Portfolio',
+  domain: 'danvegamo.com',
+  author: 'David Vega - Danvegamo',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'My personal portfolio, where the magic happens and the new realities are created.',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  //twitter: 'transitive_bs',
+  github: 'Danvegamo',
+  linkedin: 'Danvegamo',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  newsletter: 'https://medium.com/@d_vega', // optional newsletter URL
+  youtube: 'channel/UCyMmbzWgp46YO0dSLM5yNTg', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
