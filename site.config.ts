b682e6b01d43359c0bc9030f5d45f5251e7ae988
David Vega <102634786+Danvegamo@users.noreply.github.com>
2023-09-07 09:58:07 -0700

    upload website

--- site.config.ts
+++ site.config.ts
@@ -3,26 +3,26 @@ import { siteConfig } from './lib/site-config'
 export default siteConfig({
   // the site's root Notion page (required)
   rootNotionPageId: '016e0ef793884b01a8bee4d024a6591c',
-  
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'David Vega Portfolio',
+  name: 'David Vega',
   domain: 'danvegamo.com',
-  author: 'David Vega - Danvegamo',
+  author: 'David Vega',
 
   // open graph metadata (optional)
-  description: 'My personal portfolio, where the magic happens and the new realities are created.',
+  description: 'Portfolio of David Vega',
 
   // social usernames (optional)
-  //twitter: 'transitive_bs',
+  // twitter: 'transitive_bs',
   github: 'Danvegamo',
   linkedin: 'Danvegamo',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
-  newsletter: 'https://medium.com/@d_vega', // optional newsletter URL
+  medium: 'https://medium.com/@d_vega', // optional medium URL
+  // newsletter: '#', // optional newsletter URL
   youtube: 'channel/UCyMmbzWgp46YO0dSLM5yNTg', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
@@ -51,16 +51,20 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  //navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About of me',
+     pageId: 'e56902269a40450295f9c86cdbe44e31'
+     },
+    {
+      title: 'Contact',
+      pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+    },
+    {
+      title: 'Blog',
+      url: 'https://medium.com/@d_vega'
+    }
+   ]
 })
